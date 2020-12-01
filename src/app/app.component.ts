1754e6fda662cf053900cd0173ad19ef659e3e9f
cynthia <201810831@tese.edu.mx>
2020-11-30 19:24:14 -0600

    Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practicasBasicaspipes';
+  name = 'sheldon';
+  myArray = [1, 2, 3, 4, 5, 6, 7, 8,];
+  PI = Math.PI;
+  myNum = 0.589;
+  salary = 3500.5;
+  nerd = { 
+    name: 'Sheldon', 
+    alias: 'Shelly', 
+    song: 'Soft Kitty', 
+    skills: ['eidetic memory', 'makes people nervous'], 
+    youtubeChannel: 'Fun With Flags', 
+    address: { 
+    street: 'Elm Street', 
+    number: 3, 
+   city: 'Pasadena' 
+  } 
+}; 
+myDate = new Date();
+video = 'o-0ygW-B_gI';
 }
+
